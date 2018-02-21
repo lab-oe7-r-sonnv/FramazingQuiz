1ed0be7e3c12d6ab1c2c057961db1f8f4d6b13d2
vtson <vts.vuthanhson@gmail.com>
2018-02-21 16:47:18 +0700

    signup

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem "faker"
 gem "bootstrap-sass", "3.3.7"
 gem "bcrypt"
 gem "jquery-rails"
+gem "devise"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable,
+    :confirmable
+
   has_many :topics, dependent: :destroy, foreign_key: :creator_id,
     inverse_of: :creator
   has_many :lessons, through: :topics
 
-  has_secure_password
+  enum sex: %i(male female)
+  enum type: %i(student teacher)
 end
